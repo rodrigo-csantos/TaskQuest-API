0022f6e49c5026bd01457c0425758ce5cf713b7a
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-05-21 16:17:58 -0300

    correção da importação do model tasks

--- src/services/taskHandlers.service.js
+++ src/services/taskHandlers.service.js
@@ -1,22 +1,22 @@
-const {taskHandler} = require('../models')
+const {tasks} = require('../models')
 
 const listAllTasks = async () => {
-    const allTasks = await taskHandler.findAll()
+    const allTasks = await tasks.findAll()
     return allTasks
 }
 
 const taskDetails =  async (id)=> {
-    const taskDetailsById = await taskHandler.findOne({where:{id}})
+    const taskDetailsById = await tasks.findOne({where:{id}})
     return taskDetailsById
 }
 
 const createTask = async (taskData)=> {
-    const newTask = await taskHandler.create(taskData)
+    const newTask = await tasks.create(taskData)
     return newTask
 }
 
 const updateTaskStatus = async (taskStatus, id)=> {
-    const taskUpdated = await taskHandler.findOne({where: {id}})
+    const taskUpdated = await tasks.findOne({where: {id}})
     taskUpdated.status = taskStatus
     await taskUpdated.save()
     
@@ -24,7 +24,7 @@ const updateTaskStatus = async (taskStatus, id)=> {
 }
 
 const deleteTask = async (id)=> {
-    const deletedTask = await taskHandler.destroy({where: {id}})
+    const deletedTask = await tasks.destroy({where: {id}})
     return deletedTask
 }
 
