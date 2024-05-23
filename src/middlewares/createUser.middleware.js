222c67ad4252220fd293ac0e9359d3a2aca99678
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-05-23 16:33:54 -0300

    add validações e middlewares para cadastro de usuários

---
+++ src/middlewares/createUser.middleware.js
@@ -0,0 +1,17 @@
+const { userData } = require('../validations/createUserValidations');
+
+const validateUserData = (req, res, next) => {
+	const { error } = userData.validate(req.body, { abortEarly: false });
+	if (error) {
+		return res.status(400).json({
+			type: error.details.map((detail) => detail.type),
+			message: error.details.map((detail) => detail.message),
+		});
+	}
+
+	next();
+};
+
+module.exports = {
+	validateUserData,
+};
